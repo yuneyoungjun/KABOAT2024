9e6e811090e8d78ed5e4205f6cda4dbfa1ce4798
als8921 <als8921@naver.com>
2024-08-26 20:35:46 +0900

    Boat Postion 클래스화

--- src/kaboat2024/src/Control/Autonomous.py
+++ src/kaboat2024/src/Control/Autonomous.py
@@ -9,17 +9,14 @@ from math import ceil
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import message_filters
-from AutonomousBoatController import AutonomousBoatController
-
+from modules.AutonomousBoatController import AutonomousBoatController
+from main import Boat
 # 초기 거리 데이터 설정
 distances = []
-angles = []
-waypoints = []  # Waypoint 데이터를 저장할 리스트
+angles = np.radians(np.arange(360)) 
 waypoint_angle = 0  # Waypoint 각도를 저장할 리스트
 threshold = 100.0  # 특정 거리 임계값 설정 (예: 100.0 센티미터)
 
-gps_position = []
-heading_angle = 0
 
 Goal_Psi = 0
 Goal_Distance = 0
@@ -31,7 +28,7 @@ goal_threshold = 4 # 도착 거리
 def normalize_angle(angle): return (angle + 180) % 360 - 180
 # 시각화 함수
 def update(frame):
-    global distances, angles, waypoints, waypoint_angle, Goal_Psi, Goal_Distance, cost_function_values, psi_error_publisher, psi_error_history
+    global waypoint_angle, Goal_Psi, Goal_Distance, cost_function_values, psi_error_publisher, psi_error_history
 
     # 첫 번째 subplot: 거리 데이터 시각화
     ax.clear()
@@ -42,12 +39,12 @@ def update(frame):
     ax.set_theta_direction(-1)
 
     # 점으로만 표시
-    ax.scatter(angles, distances, color='blue', s=5)  # 라이다 데이터 점으로 표시
+    ax.scatter(angles, Boat.scan, color='blue', s=5)  # 라이다 데이터 점으로 표시
 
-    if(len(distances) == 0):
+    if(len(Boat.scan) == 0):
         return
 
-    safe_ld = autonomousController.calculate_safe_zone(distances)
+    safe_ld = autonomousController.calculate_safe_zone(Boat.scan)
     cost_function = autonomousController.calculate_optimal_psi_d(safe_ld, int(Goal_Psi))
     psi_error = sorted(cost_function, key=lambda x: x[1])[0][0]
     psi_error = normalize_angle(psi_error)
@@ -59,7 +56,7 @@ def update(frame):
             tauX = min((Goal_Distance**3) + 120, 500)
 
         else:
-            tauX_dist = min(3 * distances[0] ** 2, 300)
+            tauX_dist = min(3 * Boat.scan[0] ** 2, 300)
             tauX_psi = 200/(abs(psi_error) + 1)
             tauX = min(tauX_dist + tauX_psi, 500)
         # psi_error 시각화
@@ -78,26 +75,26 @@ def update(frame):
     ax.fill(angles, safe_ld, color=[0, 1, 0, 0.2])  # 안전 구역 표시
 
     # Waypoint 데이터 표시
-    if waypoints:
-        waypoint_x = [(point[0] - gps_position[0]) for point in waypoints]
-        waypoint_y = [(point[1] - gps_position[1]) for point in waypoints]
+    if Boat.waypoints:
+        waypoint_x = [(point[0] - Boat.position[0]) for point in Boat.waypoints]
+        waypoint_y = [(point[1] - Boat.position[1]) for point in Boat.waypoints]
 
-        if(goal_passed(waypoints[0][0], waypoints[0][1])):
-            waypoints.pop(0)
+        if(goal_passed(Boat.waypoints[0][0], Boat.waypoints[0][1])):
+            Boat.waypoints.pop(0)
 
 
-        Goal_Psi = np.arctan2(waypoint_x[0], waypoint_y[0]) * 180 / np.pi - heading_angle
+        Goal_Psi = np.arctan2(waypoint_x[0], waypoint_y[0]) * 180 / np.pi - Boat.psi
         Goal_Psi = normalize_angle(Goal_Psi)
         Goal_Distance = np.sqrt(np.power(waypoint_x[0], 2) + np.power(waypoint_y[0], 2))
 
         # Waypoint 점으로 표시
-        ax.scatter(np.radians(np.arctan2(waypoint_x, waypoint_y) * 180 / np.pi - heading_angle),
+        ax.scatter(np.radians(np.arctan2(waypoint_x, waypoint_y) * 180 / np.pi - Boat.psi),
                    np.sqrt(np.power(waypoint_x, 2) + np.power(waypoint_y, 2)),
                    color='red', label='Waypoints')
 
         # 각 Waypoint에 인덱스 번호 텍스트 추가
         for idx, (wx, wy) in enumerate(zip(waypoint_x, waypoint_y)):
-            angle = np.radians(np.arctan2(wx, wy) * 180 / np.pi - heading_angle)
+            angle = np.radians(np.arctan2(wx, wy) * 180 / np.pi - Boat.psi)
             distance = np.sqrt(np.power(wx, 2) + np.power(wy, 2))
             ax.text(angle, distance, str(idx + 1), color='black', fontsize=8, ha='center', va='bottom')
 
@@ -135,23 +132,23 @@ def goal_check():
         angle = normalize_angle(int(Goal_Psi) - 90 + i)
         r = autonomousController.BOAT_WIDTH /(2 *np.cos(np.radians(i)))
         check_ld[angle] = r
-        if(distances[angle] == 0):
+        if(Boat.scan[angle] == 0):
             continue
-        if(r > distances[angle]):
+        if(r > Boat.scan[angle]):
             isAble = False
 
     for i in range(-theta, theta + 1):
         check_ld[normalize_angle(int(Goal_Psi) + i)] = l
-        if(distances[normalize_angle(int(Goal_Psi) + i)] < l):
+        if(Boat.scan[normalize_angle(int(Goal_Psi) + i)] < l):
             isAble = False
 
     for i in range(0, 90 - theta):
         angle = normalize_angle(int(Goal_Psi) + 90 - i)
         r = autonomousController.BOAT_WIDTH /(2 *np.cos(np.radians(i)))
         check_ld[angle] = r
-        if(distances[angle] == 0):
+        if(Boat.scan[angle] == 0):
             continue
-        if(r > distances[angle]):
+        if(r > Boat.scan[angle]):
             isAble = False
     ax.fill(angles, check_ld, color=[0, 0, 1, 0.2])
 
@@ -159,40 +156,35 @@ def goal_check():
 
 def goal_passed(goal_x, goal_y):
     isPassed = False
-    if((gps_position[0] - goal_x)**2 + (gps_position[1] - goal_y)**2 < goal_threshold ** 2):
+    if((Boat.position[0] - goal_x)**2 + (Boat.position[1] - goal_y)**2 < goal_threshold ** 2):
         isPassed = True
     return isPassed
 
 def laser_scan_callback(data):
-    global distances, angles
     distances = np.array(data.ranges)  # 수신한 거리 데이터
     distances[distances > threshold] = 0  # 임계값 초과 시 0으로 변경
-    angles = np.linspace(data.angle_min, data.angle_max, len(distances))  # 각도 생성
+    Boat.scan = distances
 
 def simulator_laser_scan_callback(data):
-    global distances, angles
     distances = np.array(data.data)  # 수신한 거리 데이터
     distances[distances > threshold] = 0  # 임계값 초과 시 0으로 변경
-    angles = np.radians(np.arange(len(distances)))  # 각도 생성 (0 ~ 360도)
+    Boat.scan = distances
 
 def waypoint_callback(data):
-    global waypoints
     if(data.point.z == -1):
-        waypoints = []
+        Boat.waypoints = []
     else:
-        waypoints.append([data.point.x, data.point.y])
+        Boat.waypoints.append([data.point.x, data.point.y])
 
 def waypoint_angle_callback(data):
     global waypoint_angle
     waypoint_angle = data.data  # 각도 데이터 추가
 
 def gps_callback(data):
-    global gps_position
-    gps_position = data.data  # GPS 위치 업데이트 (예: [x, y])
+    Boat.position = data.data
 
 def imu_callback(data):
-    global heading_angle
-    heading_angle = data.data  # 방위각 업데이트
+    Boat.psi = data.data
 
 def listener(is_simulator=False):
     global psi_error_publisher
